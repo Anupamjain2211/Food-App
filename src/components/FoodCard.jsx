62c4ece41734aa2aa8bf54fbafd3b9c637c6d0de
anupam <anupamjain177@gmail.com>
2024-05-28 16:08:06 -0400

    first commit

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { AiFillStar } from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/slices/CartSlice";
 
-const FoodCard = ({id, name, price, img, desc, rating}) => {
+const FoodCard = ({ id, name, price, img, desc, rating }) => {
+  const dispatch = useDispatch();
   return (
     <div className="font-bold w-[250px] p-5 bg-white  flex flex-col rounded-lg gap-2">
       <img
@@ -13,14 +16,20 @@ const FoodCard = ({id, name, price, img, desc, rating}) => {
         <h2>{name}</h2>
         <span className="text-green-500">₹{price}</span>
       </div>
-      <p className="text-sm font-normal">
-        {desc.slice(0,50)}...
-      </p>
+      <p className="text-sm font-normal">{desc.slice(0, 50)}...</p>
       <div className="flex justify-between">
         <span className="flex items-center justify-center">
           <AiFillStar className="mr-1 text-yellow-400" /> {rating}
         </span>
-        <button className="p-1 text-white bg-green-500 hover:bg-gray-600 rounded-lg text-sm">
+        <button
+          onClick={() => {
+            dispatch(
+              addToCart({ id, name, rating, price, img, qty: 1 })
+            );
+            handleToast(name);
+          }}
+          className="p-1 text-white bg-green-500 hover:bg-green-600 rounded-lg text-sm"
+        >
           Add to cart
         </button>
       </div>
